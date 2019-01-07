3b62fca5fb75e13fc340adf851456c4367ae9511
Yoni <yoniifriedman@gmail.com>
2019-01-06 21:56:24 -0500

    Started new index page

--- js/base.js
+++ js/base.js
@@ -3,3 +3,15 @@
 $('body').scrollspy({
     offset: offsetHeight
  });
+
+function triggerAnimation(){
+    new Vue({
+    el: "#app",
+        mounted() {
+            TweenMax.set(`.polygons path`, { scale: 0, opacity: 0, transformOrigin: '50% 50%' })
+            TweenMax.staggerTo(`.polygons path`, 0.25, {
+                scale: 1, opacity: 1, ease: Back.easeOut.config(0.7)
+            }, 0.03);
+        },
+    });
+}
